76a3adcbdc57693d7d93fbdb80218707dd8c42ea
Malcolm Jack <inquisitorjax@gmail.com>
2019-01-31 00:04:01 +0200

    run app

--- Windows/MainPage.xaml.cs
+++ Windows/MainPage.xaml.cs
@@ -1,25 +1,24 @@
 ﻿// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-using SampleApplication.AppServices;
 using Syncfusion.ListView.XForms.UWP;
 
 namespace SampleApplication.Windows
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage
-    {
-        public MainPage()
-        {
-            //https://developer.xamarin.com/guides/xamarin-forms/platform-features/windows/installation/universal/
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage
+	{
+		public MainPage()
+		{
+			//https://developer.xamarin.com/guides/xamarin-forms/platform-features/windows/installation/universal/
 
-            this.InitializeComponent();
+			this.InitializeComponent();
 
-            SfListViewRenderer.Init();
-            Xamarin.FormsMaps.Init(PrivateTokens.UWPMapsToken);            
-            
-            LoadApplication(new SampleApplication.App());
-        }
-    }
+			SfListViewRenderer.Init();
+			Xamarin.FormsMaps.Init(PrivateTokens.UWPMapsToken);
+
+			LoadApplication(new SampleApplication.App());
+		}
+	}
 }
